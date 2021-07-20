8413c0667dd3343226397d13b4f7a700ffbfa71f
André Silva do Nascimento <andresilvay6@gmail.com>
2021-07-19 22:56:45 -0300

    Criando pasta admin e classe PageAdmin

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Hcode\Page;
+use Hcode\PageAdmin;
 use Slim\Slim;
 
 require_once("vendor/autoload.php");
@@ -12,6 +13,13 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
